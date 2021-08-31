9bc17635204a2264122534eff9e73f26279b181c
William <guillermoalvarez92@hotmail.com>
2021-08-31 10:18:07 +0100

    update

--- app.js
+++ app.js
@@ -10,20 +10,31 @@ function init() {
     const numCells = width * width
     const cells = []
 
-    /// ### Classes
+    // ### Classes
 
     const ship = 'ship'
     const enemy = 'enemy'
     const bam = 'bam'
     const boom = 'boom'
 
+    // ### Cell contains
+
+    function containShip(index) {
+        let cellToCheck = cells[index]
+        if (cellToCheck.target.classList.contain('ship') === true) {
+            console.log('yes')
+        }
+    }
+
     // ### Positioning
 
     //Ship
+
     const startShip = numCells - width + (width / 2 - 1)
     let currentShipPos = startShip
 
     //Enemy
+
     const startEnemy = width + 1
     let currentEnemyPos = startEnemy
     const rowsWithEnemies = 4
@@ -51,7 +62,6 @@ function init() {
         // cells[index].classList.add(enemy)
         for (let i = 0; i < width * totalRowsEnemies; i++) {
             if (i % 2 === 0 && i < width * totalRowsEnemies && i % width <= totalColumnsEnemies && i % (width * 2) < width) {
-
                 cells[index + i].classList.add(enemy)
             }
             //     // i % 2 === 0 && 
@@ -77,8 +87,7 @@ function init() {
         // cells[index].classList.remove(enemy)
         for (let i = 0; i < width * totalRowsEnemies; i++) {
             if (i % 2 === 0 && i < width * totalRowsEnemies && i % width <= totalColumnsEnemies && i % (width * 2) < width) {
-                console.log(index + i),
-                    cells[index + i].classList.remove(enemy)
+                cells[index + i].classList.remove(enemy)
 
             }
         }
@@ -101,7 +110,9 @@ function init() {
     function removeLaser(index) {
         cells[index].classList.remove(bam)
     }
+
     // Evil laser
+
     function addEvilLaser(index) {
         cells[index].classList.add(boom)
     }
@@ -144,7 +155,7 @@ function init() {
             else {
                 console.log('GAME OVER')
             }
-        }, 10000)
+        }, 3000)
     }
 
     // Laser DONE
@@ -161,19 +172,46 @@ function init() {
         }, 50)
     }
 
-    // Evil laser
-    function EvilLaserMove(index) {
+    // Evil laser move
+    function evilLaserMove(index) {
         setInterval(function interEvilLaser() {
-            if (index > width - 1) {
-                removeLaser(index),
+            if (index < (numCells - width)) {
+                removeEvilLaser(index),
                     index += width,
-                    addLaser(index)
+                    addEvilLaser(index)
             } else {
-                removeLaser(index)
+                removeEvilLaser(index)
             }
-        }, 50)
+        }, 300)
     }
 
+    // Evil laser random
+
+    function evilLaserRandom() {
+        setInterval(function interEvilLaser() {
+            const randomLaser = currentEnemyPos + (Math.floor(Math.random() * totalColumnsEnemies))
+            evilLaserMove(randomLaser)
+        }, 5000)
+    }
+
+    // function EvilLaserRandom() {
+    //     let randomLaser = cells[(Math.floor(Math.random() * totalColumnsEnemies))]
+    //     for (let i = 0; i < 10; i++) {
+    //         if (i >= currentEnemyPos && i < totalColumnsEnemies) {
+
+    //             EvilLaserMove(randomLaser)
+    //         }
+    //     }
+    // }
+
+    // Checker
+
+    // function checker() {
+    //     for (let i = 0; i < numCells - width - 1; i++) {
+    //         // Enemy destroy ship
+    //         if ()
+    //     }
+    // }
 
     // ### Grid creator (DONT TOUCH!)
 
@@ -182,26 +220,28 @@ function init() {
             const iwidth = i % width
             const irow = i % 40
             const cell = document.createElement('div')
-            cell.innerText = iwidth
+            cell.innerText = irow
             grid.appendChild(cell)
             cells.push(cell)
         }
         addShip(startShip)
         addEnemy(currentEnemyPos)
         enemyMove()
-
+        evilLaserRandom()
     }
 
     // ### Control (DONT TOUCH!)
 
     function movingShip(event) {
-        // console.log(event.keyCode)
+        console.log(event.keyCode)
         removeShip(currentShipPos)
         const key = event.keyCode
         const space = 32
         const right = 39
         const left = 37
         const h = 72
+        const t = 84
+        const randomLaser = currentEnemyPos + (Math.floor(Math.random() * totalColumnsEnemies))
 
         if (key === right && currentShipPos < numCells - 1) {
             currentShipPos++
@@ -211,7 +251,10 @@ function init() {
             console.log('BAM!')
             laserMove(currentShipPos)
         } else if (key === h) {
-            console.log(width * totalColumnsEnemies)
+            containShip(currentShipPos)
+
+        } else if (key === t) {
+            console.log(evilLaserMove(randomLaser))
         } else {
             console.log('Pong sound!')
         }
@@ -223,8 +266,9 @@ function init() {
 
     GridCreator()
 
-    document.addEventListener('keydown', movingShip)
 
+    document.addEventListener('keydown', movingShip)
+        // document.addEventListener('DOMContentLoaded', checker)
 }
 
 window.addEventListener('DOMContentLoaded', init)
